scattering_delays/R_from_MC.py: fix the calculation to handle the exponential and power-law tails correctly

scattering_delays/R_from_MC.py
@@ -2,28 +2,30 @@ import numpy as np
 import scipy.special as spspec
 
 
-def fit_exponential_tail(tt, R, N, L_scale, τ_exp ):
+def fit_exponential_tail(tt, R, N, L_scale, τ_exp):
 
-    τ_WF=tt/L_scale
+    τ_out=tt/L_scale
 
     # power where the tail diverges from the parent curve
-    P0_exp = R[np.argmin(τ_WF < τ_exp )]
+    P0_exp = R[np.argmin(τ_out < τ_exp )]
 
-    exp_fit_ind = np.flatnonzero( (τ_WF > τ_exp ) & (R<0.5*P0_exp) & (N>50))
+    exp_fit_ind = np.flatnonzero( (τ_out > τ_exp ) & (R<0.5*P0_exp) & (N>50) & np.isfinite(R))
     if len(exp_fit_ind) < 5:
-        print('fit_exponential_tail: not enough bins to fit the tail')
-        raise(ValueError())
-    G_exp = np.c_[np.ones(len(exp_fit_ind)), tt[exp_fit_ind]]
+        return R.copy(), None
+    G_exp = np.c_[np.ones(len(exp_fit_ind)), tt[exp_fit_ind]/1.e-9]
     m_exp = np.linalg.inv(G_exp.T.dot(G_exp)).dot(G_exp.T.dot(np.log(R[exp_fit_ind])))
+    # undo the xcaling on m[1]
+    m_exp[1] /= 1.e-9
 
     # Fill in the low-precision elements of the tail (including the zeros)
-    exp_fill_ind=np.flatnonzero((N<200) & (τ_WF > τ_exp ))
+    exp_fill_ind=np.flatnonzero(((N<200) | np.isnan(R)) & (τ_out > τ_exp ))
     R1=R.copy()
     R1[exp_fill_ind] = np.exp(m_exp[0]+m_exp[1]*tt[exp_fill_ind])
-    R_tail = -np.exp(m_exp[0])/m_exp[1]*np.exp(m_exp[1]*tt[-1])
+    R_tail = -np.exp(m_exp[0])/(m_exp[1])*np.exp(m_exp[1]*tt[-1])
+
     return R1, R_tail
 
-def R_from_MC(t_out, mu_s, mu_a, g, c, D_MC, τ_tail=200, τ_exp=None, skip_tail=False, L_max=1900):
+def R_from_MC(t_out, mu_s, mu_a, g, c, D_MC, τ_tail=200, τ_exp=None, skip_tail=False, L_max=1900, DEBUG=False):
 
     """
     Created on Wed July 6 13:10:51 2022
@@ -81,10 +83,9 @@ def R_from_MC(t_out, mu_s, mu_a, g, c, D_MC, τ_tail=200, τ_exp=None, skip_tail
 
     P_abs = E_abs/(D_MC['lb']-D_MC['la'])
     # interpolate to get the power as a function of time
-    R = np.interp(t_out, t_MC, P_abs) / L_scale
+    R = np.interp(t_out, t_MC, P_abs, right=np.NaN) / L_scale
     # get the data count
     #N = np.interp(t_out, t_MC, D_MC['E']/D_MC['norm'])
-
 
     R[t_out<t_MC[0]]=0
 
@@ -93,37 +94,32 @@ def R_from_MC(t_out, mu_s, mu_a, g, c, D_MC, τ_tail=200, τ_exp=None, skip_tail
         R_tail=0
         return R, R_tail
 
-    # Are we asking for values that are in the exponential decay region?
-    if (np.max(τ_out) > τ_exp):
-        # Does the MC cover the exponential decay?
-        if np.max(D_MC['lc']) > τ_exp:
-            N=np.interp(t_out, t_MC, D_MC['E']/D_MC['norm'])
-            R1, R_tail = fit_exponential_tail(t_out, R, N, L_scale, τ_exp)
-            return R1, R_tail
-
-    # We get to this point if we are not fitting an exponential.
-    if np.max(τ_out) < τ_tail:
-        R[t_out>t_MC[-1]]=0
-        R_tail=0
-        return R, R_tail
+    R1=R.copy()
 
     # Powerlaw tail fit
-    tail = (D_MC['lb'] > τ_tail) & (D_MC['la'] < L_max)
+    tail = (D_MC['lb'] > τ_tail) & (D_MC['la'] < L_max) & (D_MC['lb'] < τ_exp)
     if np.sum(tail)<2:
-        print("Not enough bins in tail")
-        raise(IndexError())
+        A_tail=None
+    else:
+        G_tail = np.exp(-mu_a * L_MC[tail])*L_MC[tail]**(-3/2);
+        d_tail = P_abs[tail];  # power per unit (nondimensional) length
+        A_tail = 1/(G_tail.dot(G_tail))*(G_tail.T.dot(d_tail))
+        power_fill_els = ((τ_out > D_MC['lc'][-1]) | np.isnan(R)) & (τ_out <= τ_exp)
+        L_fill=c*t_out[power_fill_els]
+        R1[power_fill_els] = A_tail * np.exp(-mu_a * L_fill) * L_fill**(-3/2)/L_scale
 
-    G_tail = np.exp(-mu_a * L_MC[tail])*L_MC[tail]**(-3/2);
-    d_tail = (E_abs[tail]/(D_MC['lb'][tail]-D_MC['la'][tail]));  # power per unit (nondimensional) length
-    A_tail = 1/(G_tail.dot(G_tail))*(G_tail.T.dot(d_tail));
-    #sigma_tail = np.std(d_tail - G_tail*A_tail);
-    R1=R.copy()
-    # if there are any times requested that are later than the end of the MC
-    # data, reconstruct them based on the tail fit.
-    late_els = t_out > t_MC[-1];
-    if np.any(late_els):
-        L_late=c*t_out[late_els];
-        R1[late_els]=A_tail*np.exp(-mu_a * L_late)*L_late**(-3/2)/L_scale;
+    if τ_exp is not None and np.isfinite(τ_exp) and τ_exp < τ_out[-1]:
+        N=np.interp(t_out, t_MC, D_MC['E']/D_MC['norm'], right=np.NaN)
+        R1, R_tail = fit_exponential_tail(t_out, R1, N, L_scale, τ_exp)
+        if R_tail is not None:
+            return R1, R_tail
+
+
+    # Calculate the total energy in the tail beyond the end of the requested
+    # waveform based on the attenuated power law fit
+    b0 = t_out[-1]*c;
+    R_tail = A_tail*(1-g)*mu_s*(2*np.exp(-b0*mu_a)/np.sqrt(b0) -
+                                2*np.sqrt(np.pi*mu_a)* spspec.erfc(np.sqrt(b0*mu_a)))
 
     if np.isfinite(τ_exp):
         # if there is a finite exponential decay, extrapolate its slope from the
@@ -143,13 +139,6 @@ def R_from_MC(t_out, mu_s, mu_a, g, c, D_MC, τ_tail=200, τ_exp=None, skip_tail
             return R1, R_tail
         else:
             R_tail_exp = -A_exp/K_exp
-
-    # Otherwise, calculate the total energy in the tail beyond the end of the requested
-    # waveform based on the attenuated power law fit
-    b0 = t_out[-1]*c;
-    R_tail = A_tail*(1-g)*mu_s*(2*np.exp(-b0*mu_a)/np.sqrt(b0) -
-                                2*np.sqrt(np.pi*mu_a)* spspec.erfc(np.sqrt(b0*mu_a)))
-    if np.isfinite(τ_exp):
         # if the tail goes exponential at some late time, subtract the power in the
         # t^-3/2 curve beyond this time, and add the power in the exponential curve
         b_exp = t_exp*c